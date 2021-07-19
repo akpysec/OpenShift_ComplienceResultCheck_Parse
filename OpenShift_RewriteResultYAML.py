1728c59a3c43ebf60701a4bd31e6abdf3bded7a7
AKPySec <48283299+akpysec@users.noreply.github.com>
2021-07-20 00:26:41 +0300

    Create OpenShift_RewriteResultYAML.py

---
+++ OpenShift_RewriteResultYAML.py
@@ -0,0 +1,45 @@
+import yaml
+from yaml.loader import SafeLoader
+import pandas as pd
+
+# New Dictionary for selected parameters from Checks
+parsed_checks = dict()
+
+# Specify path to ComplianceCheckResult file
+path_to_OpenShift_Audit_File = "/enter/path/here/ComplianceResultCheck.yaml"
+
+# Open the YAML file and load the YAML file
+with open(path_to_OpenShift_Audit_File) as f:
+    data = list(yaml.load_all(f, Loader=SafeLoader))
+
+
+    # Counting total Checks
+    numberOfChecks = len(data[0]["items"])   
+
+    number = 0
+
+    while number < numberOfChecks:
+        parsed_checks.update({number: [
+            data[0]["items"][number]["id"].upper().lstrip("XCCDF_ORG.SSGPROJECT.CONTENT_RULE_").replace("_", " "),
+            data[0]["items"][number]["description"],
+            data[0]["items"][number]["severity"],
+            data[0]["items"][number]["status"]]})
+        number = number + 1
+
+df = pd.DataFrame([v for k, v in parsed_checks.items()], columns =['Name', 'Description', 'Severity', 'Status'])
+
+# Setting sort by value order
+df['Severity'] = pd.Categorical(df['Severity'], ['high', 'medium', 'low'])
+
+# Changing data_frames order from high to low
+df = df.sort_values('Severity')
+
+# Filtering out only the hardening values that have registry keys & values
+df_pass = df[df['Status'].str.contains("PASS")]
+df_fail = df[df['Status'].str.contains("FAIL")]
+
+df = df_pass.merge(right=df_fail, how='outer')
+
+# print(df)
+df.to_csv('test_out.csv')
+
