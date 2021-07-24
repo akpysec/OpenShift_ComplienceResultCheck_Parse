ecf9a94c60a60383ff7fdb1ee575665b5f67da76
AKPySec <48283299+akpysec@users.noreply.github.com>
2021-07-25 00:17:20 +0300

    Update OpenShift_RewriteResultYAML.py

--- OpenShift_RewriteResultYAML.py
+++ OpenShift_RewriteResultYAML.py
@@ -1,48 +1,53 @@
+# Run with at least Python 3.8
 import yaml
 from yaml.loader import SafeLoader
 import pandas as pd
-import time 
-
-# New Dictionary for selected parameters from Checks
-parsed_checks = dict()
-
-# Specify path to ComplianceCheckResult file
-path_to_OpenShift_Audit_File = "/add/path/to/file/here/Complianceresultcheck.yaml"
-
-# Open the YAML file and load the YAML file
-with open(path_to_OpenShift_Audit_File) as f:
-    data = list(yaml.load_all(f, Loader=SafeLoader))
 
 
-    # Counting total Checks
-    numberOfChecks = len(data[0]["items"])   
+def compliance_check_result_parse(input_file: str, output_file: str):
+    
+    # New Dictionary for selected parameters from Checks
+    parsed_checks = dict()
+       
+    # Open the YAML file and load the YAML file
+    with open(input_file) as f:
+        data = list(yaml.load_all(f, Loader=SafeLoader))
+    
+        # Counting total Checks
+        number_of_checks = len(data[0]["items"])
+    
+        number = 0
+    
+        # Walrus :)
+        while (number := number + 1) < number_of_checks:
+            parsed_checks.update({number: [
+                data[0]["items"][number]["id"].upper()[34:],  # Cutting portion of a name that is not relevant
+                data[0]["items"][number]["description"],
+                data[0]["items"][number]["severity"],
+                data[0]["items"][number]["status"]]})
+    
+    # Creating DataFrame and Filling it out with parsed data
+    # Columns Creation
+    df = pd.DataFrame([v for k, v in parsed_checks.items()], columns=['Name', 'Description', 'Severity', 'Status'])
+    
+    # Setting sort by value order
+    df['Severity'] = pd.Categorical(df['Severity'], ['high', 'medium', 'low'])
+    
+    # Changing data_frames order from high to low
+    df = df.sort_values('Severity')
+    
+    # Filtering out only the hardening values that have registry keys & values
+    df_pass = df[df['Status'].str.contains("PASS")]
+    df_fail = df[df['Status'].str.contains("FAIL")]
+    
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+    
+    # Write each DataFrame to a specific sheet
+    df_pass.to_excel(writer, sheet_name='PASS')
+    df_fail.to_excel(writer, sheet_name='FAIL')
+    writer.save()
 
-    number = 0
 
-    while number < numberOfChecks:
-        parsed_checks.update({number: [
-            data[0]["items"][number]["id"].upper(),
-            data[0]["items"][number]["description"],
-            data[0]["items"][number]["severity"],
-            data[0]["items"][number]["status"]]})
-        number = number + 1
-
-# Columns Creation
-df = pd.DataFrame([v for k, v in parsed_checks.items()], columns =['Name', 'Description', 'Severity', 'Status'])
-
-# Setting sort by value order
-df['Severity'] = pd.Categorical(df['Severity'], ['high', 'medium', 'low'])
-
-# Changing data_frames order from high to low
-df = df.sort_values('Severity')
-
-# Filtering out only the hardening values that have registry keys & values
-df_pass = df[df['Status'].str.contains("PASS")]
-df_fail = df[df['Status'].str.contains("FAIL")]
-
-df = df_pass.merge(right=df_fail, how='outer')
-
-# print(df)
-
-# Write to CSV file
-df.to_csv('output.csv')
+compliance_check_result_parse(
+    input_file="path/to/input/compliancecheckresult.yaml", 
+    output_file="path/to/input/compliancecheckresult_parsed.xlsx")
